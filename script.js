ea99f8f2815be1ca6fe7ecadb7e67451a6423332
Sergey <Sergei-dg-hub@mail.ru>
2022-12-01 20:18:44 +0300

    add

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ document.getElementById('btnOver').addEventListener('click', function () {
             console.log(answerNumber)
             orderNumber++;
             orderNumberField.innerText = orderNumber;
-            answerField.innerText = `Вы загадали число ${answerNumber }?`;
+            answerField.innerText = `Вы загадали число  ${numberToPhrase(answerNumber)}?`;
         }
     }
 })
@@ -103,7 +103,7 @@ document.getElementById('btnLess').addEventListener('click', function () {
             console.log(answerNumber)
             orderNumber++;
             orderNumberField.innerText = orderNumber;
-            answerField.innerText = `Вы загадали число ${answerNumber}?`;
+            answerField.innerText = `Вы загадали число ${numberToPhrase(answerNumber)}?`;
         }
     }
 })
@@ -128,3 +128,118 @@ document.getElementById('btnEqual').addEventListener('click', function () {
         gameRun = false;
     }
 })
+
+//массив-заготовка для отображенимя чисел прописью
+const names = new Array();
+names[0] = "ноль";
+names[1] = "один";
+names[2] = "два";
+names[3] = "три";
+names[4] = "четыре";
+names[5] = "пять";
+names[6] = "шесть";
+names[7] = "семь";
+names[8] = "восемь";
+names[9] = "девять";
+names[10] = "десять";
+names[11] = "одиннадцать";
+names[12] = "двенадцать";
+names[13] = "тринадцать";
+names[14] = "четырнадцать";
+names[15] = "пятнадцать";
+names[16] = "шестнадцать";
+names[17] = "семнадцать";
+names[18] = "восемнадцать";
+names[19] = "девятнадцать";
+names[20] = "двадцать";
+names[30] = "тридцать";
+names[40] = "сорок";
+names[50] = "пятьдесят";
+names[60] = "шестьдесят";
+names[70] = "семьдесят";
+names[80] = "восемьдесят";
+names[90] = "девяносто";
+names[100] = "сто";
+names[200] = "двести";
+names[300] = "триста";
+names[400] = "четыреста";
+names[500] = "пятьсот";
+names[600] = "шестьсот";
+names[700] = "семьсот";
+names[800] = "восемьсот";
+names[900] = "девятьсот";
+
+// функция преобразовывает цифровое число в число прописью
+function numberToPhrase(number) {
+  let result = "";
+  let residue = 0; // остаток
+  let digit = 0; // разряд (10, 100)
+  
+  // обработка чисел от 0 до 20
+  if (number >= 0 && number <= 20) {
+    result = names[number];
+    console.log(result)
+  }
+  // для отрицательных
+  else if (number < 0 && number >= -20) {
+    result = "минус " + names[Math.abs(number)];
+  }
+  // ...
+  // обработка чисел от 21 до 99
+  else if (number > 20 && number < 100) {
+    residue = number % 10;
+    if (residue === 0) {
+      result = names[number];
+    } else {
+      digit = number - residue;
+      result = names[digit] + " " + names[residue];
+    }
+  }
+  // для отрицательных
+  else if (number < -20 && number > -100) {
+    residue = Math.abs(number) % 10;
+    if (residue === 0) {
+      result = "минус " + names[Math.abs(number)];
+    } else {
+      digit = Math.abs(number) - residue;
+      result = "минус " + names[digit] + " " + names[residue];
+      console.log(result);
+    }
+  }
+  // ...
+  // обработка числа от 100 до 999
+  else if (number >= 100 && number < 1000) {
+    residue = number % 100;
+    digit = number - residue;
+    if (residue === 0) {
+      result = names[digit];
+    } else if (residue % 10 === 0) {
+      result = names[digit] + " " + names[residue];
+    } else if (residue > 0 && residue <= 20) {
+      result = names[digit] + " " + names[residue];
+    } else {
+      result = names[digit] + " ";
+      digit = residue - (residue % 10);
+      result += names[digit] + " " + names[residue % 10];
+    }
+  }
+  // для отрицательных
+  else if (number <= -100 && number > -1000) {
+    residue = Math.abs(number) % 100;
+    digit = Math.abs(number) - residue;
+    if (residue === 0) {
+      result = "минус " + names[digit];
+    } else if (residue % 10 === 0) {
+      result = "минус " + names[digit] + " " + names[residue];
+    } else if (residue > 0 && residue <= 20) {
+      result = "минус " + names[digit] + " " + names[residue];
+    } else {
+      result = "минус " + names[digit] + " ";
+      digit = residue - (residue % 10);
+      result += names[digit] + " " + names[residue % 10];
+    }
+  }
+  // ...
+  return result;
+}
+// ...
